a82168489ada1825ad2462e8fcb59f9979ea0ebd
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 00:34:05 -0200

    Configura a conexao com o banco de dados postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |x| 
+  article = Article.create(title: "Meu artigo #{x}", 
+                          body: "Este é o conteudo *#{x}* do meu artigo *#{x}* ## Fim")
+  ArticleRendererWorker.new.perform(article.id)
+end
