5a75778d7cf776fd38df3a09669902263884af06
mjunior <mauriciojs.junior@gmail.com>
2019-01-18 23:33:49 -0200

    Testa o renderer de markdown

--- spec/workers/article_renderer_worker_spec.rb
+++ spec/workers/article_renderer_worker_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
+require 'sidekiq/testing'
 RSpec.describe ArticleRendererWorker, type: :worker do
   context "When a message arrives" do
+    let(:article) { create(:article)}
     it 'renderer correct html' do
-      
+      Sidekiq::Testing.inline! do
+        ArticleRendererWorker.perform_async(article.id)
+        expect(article.reload.body_rendered).to eql("<p>This is <em>bongos</em>, indeed.</p>\n")  
+      end
     end
   end
 end
