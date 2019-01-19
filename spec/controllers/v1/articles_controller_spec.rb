0e34a0d79b70c936324477ad986033eed335600e
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 20:16:33 -0200

    Corrige apontamentos do rubocop

--- spec/controllers/v1/articles_controller_spec.rb
+++ spec/controllers/v1/articles_controller_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe V1::ArticlesController, type: :controller do
-
-  describe "GET #new" do
-    context "when signed user" do   
+  describe 'GET #new' do
+    context 'when signed user' do
       before(:each) do
-        user = create (:user)
+        user = create(:user)
         sign_in user
       end
-      
-      it "returns http success" do
+      it 'returns http success' do
         get :new
         expect(response).to have_http_status(:success)
       end
     end
 
-    context "when not signed user" do
-      it "return http status 302 " do
+    context 'when not signed user' do
+      it 'return http status 302 ' do
         get :new
         expect(response).to have_http_status(302)
       end
-    end    
+    end
   end
 end
