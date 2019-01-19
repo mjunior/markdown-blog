6a44d28e495faf75ebb862ee4f91aaa8439a047a
mjunior <mauriciojs.junior@gmail.com>
2019-01-18 23:34:05 -0200

    Implementa o renderer de markdown

--- app/workers/article_renderer_worker.rb
+++ app/workers/article_renderer_worker.rb
@@ -2,6 +2,10 @@ class ArticleRendererWorker
   include Sidekiq::Worker
 
   def perform(article_id)
-    # Do something
+    article = Article.find_by(id: article_id)
+    return if article.nil?
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,{})
+    html = markdown.render(article.body)
+    article.update!(body_rendered: html)
   end
 end
