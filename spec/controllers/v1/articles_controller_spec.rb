ed2bf53a8991b893d66c5e7bd0620a0c9c7ea76d
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 14:13:12 -0200

    Testa a autenticacao do controller de artigos

--- spec/controllers/v1/articles_controller_spec.rb
+++ spec/controllers/v1/articles_controller_spec.rb
@@ -2,18 +2,24 @@ require 'rails_helper'
 
 RSpec.describe V1::ArticlesController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe "GET #new" do
+    context "when signed user" do   
+      before(:each) do
+        user = create (:user)
+        sign_in user
+      end
+      
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+    context "when not signed user" do
+      it "return http status 302 " do
+        get :new
+        expect(response).to have_http_status(302)
+      end
+    end    
   end
-
 end
