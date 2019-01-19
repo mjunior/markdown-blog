c3b1afae1f23ff33a1af58fa43c723cb0b041502
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 02:59:49 -0200

    Cria pagina para cadastro de artigos

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -6,4 +6,23 @@ class V1::ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      @article.render_body
+      redirect_to v1_article_path(@article), notice: 'Foi'
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :body, presence: true
+
+  def render_body 
+    ArticleRendererWorker.perform_async(self.id)
+  end
 end
