9328e3f3d67591846333a0510161f50fc0635361
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 13:59:07 -0200

    Testa home controller

---
+++ spec/controllers/v1/home_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe V1::HomeController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+  let(:article) { create(:article) }
+    it "returns http success" do
+      get :show, params: { slug: article.slug }  
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns 404'
+  end
+
+end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     sequence(:title) { |n| "Title #{n}" }
     body { "This is *bongos*, indeed." }
+    author { create(:user) }
   end
 end
