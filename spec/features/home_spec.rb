5c4e49dcfebf7208279381848c313ebbbfd6b4e0
mjunior <mauriciojs.junior@gmail.com>
2019-01-19 22:59:26 -0200

    Testa comportamento da pagina inicial

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Home", type: :feature do
+  describe "When access the index" do
+    before do
+      create_list(:article, 10)
+      Article.all.each do |article|
+        ArticleRendererWorker.new.perform(article.id)
+      end
+    end
+    it 'render the title Todos os artigos' do
+      visit(root_path)
+      expect(find('h1').text).to match(/Todos os artigos/)
+    end
+    it 'render all articles' do
+      visit(root_path)
+      expect(find_all('.article__title').size).to eql(10)
+    end
+  end
+end
