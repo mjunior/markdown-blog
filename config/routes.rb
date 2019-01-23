f1e5f512d0565a8c0fd0af4ed815c5004196177f
mjunior <mauriciojs.junior@gmail.com>
2019-01-22 23:59:35 -0200

    Configura o sidekiq web para desenvolvimento

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   root 'v1/home#index'
   devise_for :users
   
@@ -6,5 +9,5 @@ Rails.application.routes.draw do
     resources :articles, only: [:new, :show, :create]
     get '/', to: 'home#index'
     get '/:slug', to: 'home#show', as: :show_article
-  end
+  end 
 end
